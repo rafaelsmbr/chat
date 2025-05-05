efa3334bdd84776781a579a933066a75cb0a37ab
Rafael Soares <rafaelsm.br@gmail.com>
2025-05-05 14:21:06 -0300

    Update download_model.py

--- download_model.py
+++ download_model.py
@@ -1,5 +1,5 @@
 from sentence_transformers import SentenceTransformer
 
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
-model.save("/app/models/all-mpnet-base-v2")
-print("Modelo salvo em /app/models/all-mpnet-base-v2")
+model.save("./local_all_mpnet_base_v2")
+print("Modelo salvo em all-mpnet-base-v2")
